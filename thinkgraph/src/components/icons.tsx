ee3f614b25885fe4854e4ddb4f4d3fa5fa9d39d9
Anilkondla-tech <anil.kondla@gmail.com>
2026-06-29 23:30:37 +0530

    fix: remove duplicate IconCheck export

--- thinkgraph/src/components/icons.tsx
+++ thinkgraph/src/components/icons.tsx
@@ -152,9 +152,3 @@ export const IconExternal = (p: P) => (
     <path d="M18 13v6a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V8a2 2 0 0 1 2-2h6" />
   </svg>
 );
-
-export const IconCheck = (p: P) => (
-  <svg {...base} {...p}>
-    <path d="M20 6 9 17l-5-5" />
-  </svg>
-);
